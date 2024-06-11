084a00a378350966f98c2f41896a7ad7ea17b941
Tarcisio Couto <tarcisiocouto@MacBook-Air-de-Tarcisio.local>
2024-06-11 16:30:40 -0300

    Added WhatsApp button with delayed appearance and smooth animation using react-icons and Tailwind CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import WhatsappButton from "@/components/WhatsappBtn";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             {children}
           </main>
         </div>
+        <WhatsappButton />
         <Footer />
       </body>
     </html>
---
+++ src/components/WhatsappBtn.tsx
@@ -0,0 +1,29 @@
+// components/WhatsappButton.js
+"use client"
+import { useEffect, useState } from 'react';
+import { FaWhatsapp } from 'react-icons/fa';
+
+export default function WhatsappButton() {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsVisible(true);
+        }, 4000); // 4 segundos
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div className={`fixed bottom-4 right-4 transition-opacity duration-500 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+            <a
+                href="https://wa.me/yourphonenumber"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center justify-center w-16 h-16 bg-green-500 rounded-full shadow-lg text-white text-2xl hover:bg-green-600"
+            >
+                <FaWhatsapp size={32} /> {/* Usando o ícone do WhatsApp */}
+            </a>
+        </div>
+    );
+}
